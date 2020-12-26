0503159684e8122908151fd2a654e82129f7f378
vinraelll <53097647+vinraelll@users.noreply.github.com>
2020-12-26 16:24:49 +0200

    some corrections before send as homework

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function(){
-
   var mixer = mixitup('.gallery__inner');
+
+  $('#filter-tourists').click(function(){
+    $('#serfer').removeClass('gallery__item--no-margin-right');
+  });
+
+  $('#filter-all').click(function(){
+    $('#serfer').addClass('gallery__item--no-margin-right');
+  });
 
 });
