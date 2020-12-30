1c0ea136e6862d70a3e06d5254af8360c877c46d
vinraelll <53097647+vinraelll@users.noreply.github.com>
2020-12-30 17:11:00 +0200

    release ver.1

--- js/main.js
+++ js/main.js
@@ -20,4 +20,13 @@ $(function(){
     dots: true,
   });
 
+//   function myMap() {
+//     const mapCanvas = document.getElementById("map");
+//     const mapOptions = {
+//         center: new google.maps.LatLng(51.5, -0.2),
+//         zoom: 10
+//     };
+//     const map = new google.maps.Map(mapCanvas, mapOptions);
+// }
+
 });
