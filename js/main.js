7da044a318c196ffcaafb44262ea5ac381e92a3a
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-01-09 04:51:19 +0200

    corrections v2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function(){
 
+  $('.burger').on('click', function(e) {
+    e.preventDefault();
+  
+    $('.burger').toggleClass("active");
+    $('.menu').toggleClass("active");
+  
+  });
+
   const anchors = document.querySelectorAll('a[href*="#"]')
 
   for (let anchor of anchors) {
@@ -18,9 +26,9 @@ $(function(){
     dots: true,
   });
   
-  $('.menu__btn').on('click', function(){
+  $('.burger__btn').on('click', function(){
     $('.menu').toggleClass('menu--active')
   })
-  
+
   const mixer = mixitup('.gallery__inner');
 });
